c6b8de81c869f7be5c2f2d56b0c003b4fdc7996a
gavri <gfernandez@rednovalabs.com>
2017-05-28 18:58:19 -0500

    RandomVariableBuilders: Shorter names to construct RandomVariable objects

--- random_variable.rb
+++ random_variable.rb
@@ -6,30 +6,29 @@ class RandomVariable
     self.pmf = pmf
   end
 
-  def self.build_from_frequencies(arg)
+  def self.build_from_raw(arg)
     raw_frequencies = arg.values
     total_number_of_parts = raw_frequencies.reduce(:+)
     new(arg.each_with_object({}) {|(outcome, number_of_parts), acc| acc[outcome] = Rational(number_of_parts, total_number_of_parts)})
   end
 
-  def self.build_uniform(arg)
+  def self.build_from_uniform(arg)
     raw_frequencies = [1].cycle
-    build_from_frequencies(Hash[arg.zip(raw_frequencies)])
+    build_from_raw(Hash[arg.zip(raw_frequencies)])
   end
 
   def self.build(arg)
-    arg.is_a?(Array) ? build_uniform(arg) : build_from_frequencies(arg)
+    arg.is_a?(Array) ? build_from_uniform(arg) : build_from_frequencies(arg)
   end
 
-  def on_self(method_name, all_args)
-    first_outcome = all_args.first.outcome
-    rest_outcomes = all_args[1..-1].map(&:outcome)
-    Hash[first_outcome.send(method_name, *rest_outcomes), all_args.map(&:probability).inject(:*)]
+  def on_self(method_name, all_point_masses)
+    first, *rest = all_point_masses
+    Hash[first.outcome.send(method_name, *rest.map(&:outcome)), all_point_masses.map(&:probability).inject(:*)]
   end
 
   def method_missing(method_name, *args)
     other_random_variables = args.map {|arg| arg.kind_of?(RandomVariable) ? arg : self.class.build([arg])}
-    other_pmls = other_random_variables.map(&:pml);
+    other_pmls = other_random_variables.map(&:pml)
     cross = pml.product(*other_pmls);
     final_pmf = cross.each_with_object({}) do |point_masses, acc|
       acc.merge!(on_self(method_name, point_masses)) do |key, old_val, new_val|
@@ -51,6 +50,16 @@ class RandomVariable
     self.pmf == other.pmf
   end
 
+end
+
+module RandomVariableBuilders
+  def uniform(*args)
+    RandomVariable.build_from_uniform(*args)
+  end
+
+  def raw(*args)
+    RandomVariable.build_from_raw(*args)
+  end
 end
 
 PointMass = Struct.new("PointMass", :outcome, :probability)
--- random_variable_spec.rb
+++ random_variable_spec.rb
@@ -1,44 +1,43 @@
 require_relative 'random_variable'
-def r(*args)
-  RandomVariable.build(*args)
-end
+
 describe "Random Variable" do
+  include RandomVariableBuilders
 
   it "should create random variable from unnormalized pmf" do
-    r(1 => 3, 2 => 7).pmf.should == {1 => Rational(3, 10), 2 => Rational(7, 10)}
+    raw(1 => 3, 2 => 7).pmf.should == {1 => Rational(3, 10), 2 => Rational(7, 10)}
   end
 
   it "should create random variable of an uniform spread from array argument" do
-    r([6, 7, 8]).should == r(6 => 1, 7 => 1, 8 => 1)
+    uniform([6, 7, 8]).should == raw(6 => 1, 7 => 1, 8 => 1)
   end
 
   it "should operate probabilistically (new events are created through a single path each)" do
-    (r([100, 200]) + r([1, 2])).should == r([101, 102, 201, 202])
+    (uniform([100, 200]) + uniform([1, 2])).should == uniform([101, 102, 201, 202])
   end
 
   it "should operate probabilistically (new events are created through multiple paths)" do
-    (r([1, 2]) + r([3, 4])).should == r(4 => 1, 5 => 2, 6 => 1)
+    (uniform([1, 2]) + uniform([3, 4])).should == raw(4 => 1, 5 => 2, 6 => 1)
   end
 
   it "should work with a probabilistic choice of function" do
     add_one = 1.method(:+)
     double = 2.method(:*)
     subtract_one = ->(x){x - 1}
-    function_random_variable = r([add_one, double, subtract_one])
-    argument_random_variable = r([10, 20, 30])
-    function_random_variable[argument_random_variable].should == r([9, 11, 21, 31, 20, 29, 40, 60, 19])
+    function_random_variable = uniform ([add_one, double, subtract_one])
+    argument_random_variable = uniform([10, 20, 30])
+    function_random_variable[argument_random_variable].should == uniform([9, 11, 21, 31, 20, 29, 40, 60, 19])
   end
 
   it "should work with a probabilistic choice of function and multiple arguments" do
-    function_rv = r([->(x, y){x + y}, ->(x, y) {x - y}])
-    first_arg_rv = r([100, 200])
-    second_arg_rv = r([1, 2])
-    function_rv[first_arg_rv, second_arg_rv].should == r([101, 102, 201, 202, 99, 98, 199, 198])
+    function_rv = uniform([->(x, y){x + y}, ->(x, y) {x - y}])
+    first_arg_rv = uniform([100, 200])
+    second_arg_rv = uniform([1, 2])
+    function_rv[first_arg_rv, second_arg_rv].should == uniform([101, 102, 201, 202, 99, 98, 199, 198])
   end
 
   it "should work on non-random variables by converting them to random variables of one outcome" do
-    actual = r([3, 4, 5]) / 2
-    expected = r(1 => 1, 2 => 2)
+    actual = uniform([3, 4, 5]) / 2
+    expected = raw(1 => 1, 2 => 2)
     actual.should == expected
   end
 
